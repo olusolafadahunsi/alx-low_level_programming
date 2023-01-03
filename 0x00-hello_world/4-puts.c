fe290f931abd98e4c7bf8a6b0149d47dd8898578
olusolafadahunsi <o.o.fadahunsi@gmail.com>
2023-01-03 06:49:32 -0800

    Hot fix

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 /**
- * Main: Entry point
- *
- * Description: C programme using put function to print a string
- *
- * Return: Always O (success)
- *
+ * main:Entry point
+ * Description: 'Uses put function to print a string'
+ * Return: Always 0 (sucessful)
  *
  */
 int main(void)
 {
-puts("Programming is like building a multilingual puzzle\n");
+puts("\"Programming is like building a multilingual puzzle");
 return (0);
 }
